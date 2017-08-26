a935248ae0775ccfda8a8d493d1fd0bd16ed9113
Arthur Vigan <arthur.vigan@lam.fr>
2017-08-26 16:06:21 +0200

    Removed manual path configuration from example code
    Assumes that the package has been installed with python setup.py
    
    Close #20.

--- examples/standard_ifs_reduction.py
+++ examples/standard_ifs_reduction.py
@@ -1,11 +1,9 @@
 import sys
-path = '/Users/avigan/Work/GitHub/pySPHERE/'
-if path not in sys.path:
-    sys.path.append(path)
 import pysphere.IFS as IFS
 
 
 root_path = '/Users/avigan/data/pySPHERE-test/IFS/'
+root_path = '/Users/avigan/Desktop/fourier_dir=x_Ncycle=05_ifs_YJH'
 
 red = IFS.IFSReduction(root_path)
 
@@ -20,20 +18,20 @@ red = IFS.IFSReduction(root_path)
 # red.clean()
 
 # completely manual reduction (full control)
-red.sort_files()
-red.sort_frames()
-red.check_files_association()
-red.sph_ifs_cal_dark(silent=True)
-red.sph_ifs_cal_detector_flat(silent=True)
-red.sph_ifs_cal_specpos(silent=True)
-red.sph_ifs_cal_wave(silent=True)
-red.sph_ifs_cal_ifu_flat(silent=True)
-red.sph_ifs_preprocess_science(subtract_background=True, fix_badpix=True, correct_xtalk=True,
-                               collapse_science=False, collapse_type='mean', coadd_value=2,
-                               collapse_psf=True, collapse_center=True)
-red.sph_ifs_preprocess_wave()
-red.sph_ifs_science_cubes(postprocess=True, silent=True)
-red.sph_ifs_wavelength_recalibration(high_pass=False, display=False)
-red.sph_ifs_star_center(high_pass=False, display=False)
-red.sph_ifs_combine_data(cpix=True, psf_dim=80, science_dim=290, save_scaled=False)
-red.sph_ifs_clean(delete_raw=False, delete_products=False)
+# red.sort_files()
+# red.sort_frames()
+# red.check_files_association()
+# red.sph_ifs_cal_dark(silent=True)
+# red.sph_ifs_cal_detector_flat()
+# red.sph_ifs_cal_specpos(silent=True)
+# red.sph_ifs_cal_wave(silent=True)
+# red.sph_ifs_cal_ifu_flat(silent=False)
+# red.sph_ifs_preprocess_science(subtract_background=True, fix_badpix=True, correct_xtalk=True,
+#                                collapse_science=True, collapse_type='mean', coadd_value=2,
+#                                collapse_psf=True, collapse_center=True)
+# red.sph_ifs_preprocess_wave()
+# red.sph_ifs_science_cubes(postprocess=True, silent=True)
+# red.sph_ifs_wavelength_recalibration(high_pass=False, display=False)
+# red.sph_ifs_star_center(high_pass=False, display=False)
+# red.sph_ifs_combine_data(cpix=True, psf_dim=80, science_dim=290, save_scaled=False)
+# red.sph_ifs_clean(delete_raw=False, delete_products=False)
