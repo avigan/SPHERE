21cd73be18cbea8e421e1e3c3333e3c4474fd405
Arthur Vigan <arthur.vigan@lam.fr>
2019-08-23 08:44:50 +0200

    Add calls to new reduction functions

--- examples/irdis_spectro_reduction.py
+++ examples/irdis_spectro_reduction.py
@@ -4,8 +4,18 @@ reduction = IRDIS.SpectroReduction('/Users/avigan/data/VLTPF-test-target/IRD/LSS
 
 # reduction.sort_files()
 # reduction.sort_frames()
-reduction.check_files_association()
+# reduction.check_files_association()
 
 # reduction.sph_ird_cal_dark()
 # reduction.sph_ird_cal_detector_flat()
 # reduction.sph_ird_wave_calib()
+
+# reduction.sph_ird_preprocess_science(subtract_background=True, fix_badpix=True, 
+#                                      collapse_science=True, collapse_psf=True,
+#                                      collapse_center=True)
+
+# reduction.sph_ird_star_center(high_pass=False, display=True, save=True)
+# reduction.sph_ird_wavelength_recalibration(fit_scaling=True, display=True, save=True)
+reduction.sph_ird_combine_data(cpix=True, psf_dim=80, science_dim=300, 
+                               correct_mrs_chromatism=False, shift_method='fft', 
+                               manual_center=None, skip_center=False)
