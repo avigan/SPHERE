52d26f543f6a03fa925e3ed19438487f45213a64
Arthur Vigan <arthur.vigan@lam.fr>
2019-08-23 19:53:00 +0200

    Improve example reduction

--- examples/irdis_spectro_reduction.py
+++ examples/irdis_spectro_reduction.py
@@ -2,6 +2,17 @@ import vltpf.IRDIS as IRDIS
 
 reduction = IRDIS.SpectroReduction('/Users/avigan/data/VLTPF-test-target/IRD/LSS/')
 
+#
+# full reduction
+#
+reduction.config['combine_science_dim'] = 300
+reduction.config['clean'] = True
+
+reduction.full_reduction()
+
+#
+# manual reduction
+#
 # reduction.sort_files()
 # reduction.sort_frames()
 # reduction.check_files_association()
@@ -16,8 +27,8 @@ reduction = IRDIS.SpectroReduction('/Users/avigan/data/VLTPF-test-target/IRD/LSS
 
 # reduction.sph_ird_star_center(high_pass=False, display=True, save=True)
 # reduction.sph_ird_wavelength_recalibration(fit_scaling=True, display=True, save=True)
-reduction.sph_ird_combine_data(cpix=True, psf_dim=80, science_dim=300, 
-                               correct_mrs_chromatism=True, split_posang=True,
-                               shift_method='fft', manual_center=None, skip_center=False)
+# reduction.sph_ird_combine_data(cpix=True, psf_dim=80, science_dim=300, 
+#                                correct_mrs_chromatism=True, split_posang=True,
+#                                shift_method='fft', manual_center=None, skip_center=False)
 
 # reduction.sph_ird_clean(delete_raw=False, delete_products=False)
