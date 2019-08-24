cff06b930d3ba90c609885439f00b4bc636e8495
Arthur Vigan <arthur.vigan@lam.fr>
2019-08-24 16:59:05 +0200

    Use pathlib.Path in ReductionPath class
    Ticket #62

--- vltpf/ReductionPath.py
+++ vltpf/ReductionPath.py
@@ -1,7 +1,9 @@
 import os
 
+from pathlib import Path
 
-class Path(object):
+
+class ReductionPath(object):
     '''
     Reduction path class
 
@@ -13,15 +15,15 @@ class Path(object):
     ##################################################
     
     def __init__(self, path):
-        self._root = path
+        self._root = Path(path).expanduser()
 
         # update all subpaths
-        self._raw = os.path.join(self._root, 'raw/')
-        self._calib = os.path.join(self._root, 'calib/')
-        self._sof = os.path.join(self._root, 'sof/')
-        self._tmp = os.path.join(self._root, 'tmp/')
-        self._preproc = os.path.join(self._root, 'preproc/')
-        self._products = os.path.join(self._root, 'products/')
+        self._raw      = self._root / 'raw'
+        self._calib    = self._root / 'calib'
+        self._sof      = self._root / 'sof'
+        self._tmp      = self._root / 'tmp'
+        self._preproc  = self._root / 'preproc'
+        self._products = self._root / 'products'
 
         # create directories
         self.create_subdirectories()
@@ -31,7 +33,7 @@ class Path(object):
     ##################################################
     
     def __repr__(self):
-        return self._root
+        return str(self._root)
     
     ##################################################
     # Properties
@@ -43,15 +45,15 @@ class Path(object):
 
     @root.setter
     def root(self, path):
-        self._root = os.path.expanduser(path)
+        self._root = Path(path).expanduser()
 
         # update all subpaths
-        self._raw = os.path.join(self._root, 'raw/')
-        self._calib = os.path.join(self._root, 'calib/')
-        self._sof = os.path.join(self._root, 'sof/')
-        self._tmp = os.path.join(self._root, 'tmp/')
-        self._preproc = os.path.join(self._root, 'preproc/')
-        self._products = os.path.join(self._root, 'products/')
+        self._raw      = self._root / 'raw'
+        self._calib    = self._root / 'calib'
+        self._sof      = self._root / 'sof'
+        self._tmp      = self._root / 'tmp'
+        self._preproc  = self._root / 'preproc'
+        self._products = self._root / 'products'
 
         # create directories
         self.create_subdirectories()
@@ -86,21 +88,21 @@ class Path(object):
     
     def create_subdirectories(self):
         # create sub-directories if needed
-        if not os.path.exists(self._raw):
+        if not self._raw.exists():
             os.makedirs(self._raw)
 
-        if not os.path.exists(self._calib):
+        if not self._calib.exists():
             os.makedirs(self._calib)
 
-        if not os.path.exists(self._sof):
+        if not self._sof.exists():
             os.makedirs(self._sof)
 
-        if not os.path.exists(self._tmp):
+        if not self._tmp.exists():
             os.makedirs(self._tmp)
 
-        if not os.path.exists(self._preproc):
+        if not self._preproc.exists():
             os.makedirs(self._preproc)
 
-        if not os.path.exists(self._products):
+        if not self._products.exists():
             os.makedirs(self._products)
 
