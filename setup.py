dd004f01f534c95909695c4026e7ffd914d61a02
Arthur Vigan <arthur.vigan@lam.fr>
2020-04-06 16:50:53 +0200

    Update setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
 from setuptools import setup
 
-setup(
-    name='pysphere',
-    version='1.1',
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
+# setup
+setup(
+    name='pysphere-vlt',
+    version='1.1',
     description='Reduction and analysis code for the VLT/SPHERE instrument',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/avigan/SPHERE',
     author='Arthur Vigan',
     author_email='arthur.vigan@lam.fr',
     license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Professional Astronomers',
-        'Topic :: High-contrast Imaging and Spectroscopy',
+        'Intended Audience :: Science/Research ',
+        'Topic :: Scientific/Engineering :: Astronomy',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License'
     ],
     keywords='vlt sphere ifs irdis reduction exoplanet pipeline',
-    packages=['pyshere', 'pysphere.utils'],
+    packages=['pysphere', 'pysphere.utils', 'pysphere.IRDIS'],
     install_requires=[
         'numpy', 'scipy', 'astropy', 'pandas', 'matplotlib'
     ],
