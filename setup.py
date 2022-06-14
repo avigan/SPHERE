e069a3e1d8f033d2ccba6f51dd2318b446fb491f
Arthur Vigan <contact@astrok-software.com>
2022-06-14 16:40:09 +0200

    Fix major syntax issue in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 # setup
 setup(
     name='vlt-sphere',
-    version='1.6,
+    version='1.6.1',
     description='Reduction and analysis code for the VLT/SPHERE instrument',
     long_description=long_description,
     long_description_content_type='text/markdown',
